eb2487051c16006dff1ede383ca44a1404d5ec07
Haris818 <harismemon422@gmail.com>
2025-02-13 19:13:17 +0500

    Add files via upload

---
+++ named_function.dart
@@ -0,0 +1,33 @@
+// SIMPLE NAMED FUNCTION
+
+// void greet(String name) {
+//  print("hello, $name!");
+// }
+
+// void main(){
+//  greet("haris");
+// }
+
+
+
+// NAMED PARAMETERS FUNCTION
+
+// void introduce({String name ="Guest",int age = 10}){
+//   print("Name : $name , Age : $age");
+// }
+// void main(){
+//   introduce(name: "Haris" , age: 20);
+//   introduce(name: "ali");
+// }
+
+
+//SQUARE NAMED FUNCTION
+
+//  int add(int num){
+//    return num * num ;
+//  }
+//  void main(){
+//    print(add(2));
+//  }
+
+
